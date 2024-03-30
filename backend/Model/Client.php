c8e11d210682df75c02fde82ea57b46e2977d5fe
christ <christophe1213.com@gmail.com>
2024-03-30 17:36:33 +0300

    Creation d'une model ClientManager pour gestion Backend et ajout d'une methode affichage

--- backend/Model/Client.php
+++ backend/Model/Client.php
@@ -1,12 +1,41 @@
 <?php
-    Class Client{
+    Class ClientManager{
         private $connection;
-        public function __construct($connection){
-            $this->connection = $connection;
+        public function __construct(){
+        
+        }
+        private function connect()
+        {   
+            
+            
+            $server="localhost";
+            $user="root";
+            $mot_passe="";
+            $dbName= "compte_bancaire";
+        
+            $conn= new mysqli($server, $user, $mot_passe, $dbName);
+        
+            if($conn->connect_error){
+                die ("Echec de connection".$conn->error);
+            }
+          
+            return $conn;
         }
         public function affichage()
         {
-            
+            $this->connection=$this->connect();   
+            $query="SELECT * FROM Client;";
+            $result = $this->connection->query($query);
+            if( $result->num_rows > 0){
+                while($row = $result->fetch_assoc() ){
+                    $data[]=$row;
+                }
+            }
+            else {
+                $data=0;
+            }
+            $this->connection->close();
+            return $data;
         }
 
     }
