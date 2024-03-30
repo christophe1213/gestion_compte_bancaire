5444211f1497b0471fc24478fb46fc89a082f0ac
christ <christophe1213.com@gmail.com>
2024-03-30 16:59:37 +0300

    Installation du framework vuejs

---
+++ backend/Model/Client.php
@@ -0,0 +1,13 @@
+<?php
+    Class Client{
+        private $connection;
+        public function __construct($connection){
+            $this->connection = $connection;
+        }
+        public function affichage()
+        {
+            
+        }
+
+    }
+?>
