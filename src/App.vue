ad67cb1ed364292c9ca9a8abe481abdd5d0b349e
christ <christophe1213.com@gmail.com>
2024-04-09 20:51:24 +0300

    Test de Chartjs réussi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 
 <template>
-    <affichage></affichage>
-    <!-- <graphe></graphe> -->
+    <!-- <affichage></affichage> -->
+    <Barchart></Barchart>
+
 </template>
 <script setup>
     import { RouterLink, RouterView } from 'vue-router'
     import HelloWorld from './components/HelloWorld.vue'
     import affichage from './components/affichage.vue'
     // import graphe from '@/components/graphe.vue'
+    import Barchart from '@/components/Barchart.vue'
 
 </script>
 
